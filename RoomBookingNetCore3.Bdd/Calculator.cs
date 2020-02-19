9bfc5b95e544bd47fed2b98b4b42b4eba110e7a2
LOUKILI <youssef.loukili.ext@antalis.com>
2020-02-19 10:37:33 +0100

    Add SpecFlow to the solution

---
+++ RoomBookingNetCore3.Bdd/Calculator.cs
@@ -0,0 +1,16 @@
+
+
+namespace RoomBookingNetCore3.Bdd
+{
+    
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
---
+++ RoomBookingNetCore3.Bdd/CalculatorSteps.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RoomBookingNetCore3.Bdd
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int result;
+        private Calculator calculator = new Calculator();
+
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            //calculator.SecondNumber = number;
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            result = calculator.Add();
+            Assert.AreEqual(p0, result);
+        }
+    }
+}
